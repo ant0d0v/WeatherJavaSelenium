7853360197bef3be20c8813fb59460bece3cd32d
ant0d0v <gosu1203@gmail.com>
2023-02-01 11:33:43 +0200

    new

--- src/test/java/tests/WebSearchTest.java
+++ src/test/java/tests/WebSearchTest.java
@@ -49,10 +49,6 @@ public class WebSearchTest extends BaseTest {
         List<WebElement> elementsList = getListOfElements(by, driver);
         List<String> textList = new ArrayList<>();
 
-        // for (int i = 0; i < elementsList.size(); i++){
-        //      textList.add(elementsList.get(i).getText().toLowerCase());
-        // }
-
         for (WebElement element : elementsList) {
             textList.add(element.getText().toLowerCase());
         }
